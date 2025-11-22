607ae11e21e2ecb6649c3e7d98ae7b477ac9ce89
Sorokin Alexandr <sashasorokin754@gmail.com>
2025-11-22 22:18:09 +0300

    feat: конечная версия консольного приложения

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,24 +1,34 @@
 package org.example;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App
 {
-    public static void main( String[] args )
-    {
-        try(Scanner scanner = new Scanner(System.in)) {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Введите длину строки");
             int lineSize = scanner.nextInt();
 
-            String text = FileUtils.readFromFile("input.txt");
+            String text = null;
+            try {
+                text = FileUtils.readFromFile("input.txt");
+            } catch (IOException e) {
+                System.out.println("Ошибка при чтении файла: " + e.getMessage());
+                return;
+            }
 
             ArrayList<String> result = TextEditor.editor(text, lineSize);
 
-            FileUtils.writeToFile("output.txt", result);
-        } catch (Exception e) {
-            System.out.println("Ошибка во время чтения файла");
-        }
+            try {
+                FileUtils.writeToFile("output.txt", result);
+            } catch (IOException e) {
+                System.out.println("Ошибка при записи файла: " + e.getMessage());
+            }
 
+        } catch (Exception e) {
+            System.out.println("Ошибка ввода: " + e.getMessage());
+        }
     }
 }
